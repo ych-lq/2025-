90400dd9bfac83ad558ca464532bee9a2429c681
sdu_huangxu <164285049+sdu-huangxu@users.noreply.github.com>
2025-08-15 22:29:25 +0800

    Add files via upload

---
+++ Project 3/poseidon2_hash.sh
@@ -0,0 +1,14 @@
+circom poseidon2_hash.circom --r1cs --wasm --sym -o build
+snarkjs r1cs info build/poseidon2_hash.r1cs
+snarkjs powersoftau new bn128 14 pot14_0000.ptau -v
+snarkjs powersoftau contribute pot14_0000.ptau pot14_0001.ptau --name="first" -v
+snarkjs powersoftau prepare phase2 pot14_0001.ptau pot14_final.ptau
+snarkjs groth16 setup build/poseidon2_hash.rlcs pot14_final.ptau build/poseidon2_hash.zkey
+snarkjs zkey export verificationkey build/poseidon2_hash.zkey build/verification_key.json
+cat > input.json << 'EOF'
+{ "in0": "1", "in1": "2", "in2": "3" }
+EOF
+snarkjs wtns calculate build/poseidon2_hash_js/poseidon2_hash.wasm input.json build/witness.wtns
+ls -l build/witness.wtns
+snarkjs groth16 prove build/poseidon2_hash.zkey build/witness.wtns proof.json public.json
+snarkjs groth16 verify build/verification_key.json public.json proof.json
